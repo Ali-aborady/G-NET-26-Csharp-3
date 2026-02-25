4753fd29bd5937aca31c3786601693b937bf91fb
ali aborady <aboradyali48@gmail.com>
2026-02-25 02:03:10 +0200

    Question02

--- SecondApp/Program.cs
+++ SecondApp/Program.cs
@@ -16,6 +16,18 @@
             // Explanation: Explicit casting (int) truncates the decimal part
             // It doesn't round, just removes everything after decimal point
             #endregion
+
+            #region Q2: This code doesn’t compile. Fix it with the smallest change?
+            //=========================================================
+            //int n = 5;
+            //double d2 = n / 2;
+            //Console.WriteLine(d2);
+            //=========================================================
+            // explicit casting :-
+            //int n = 5;
+            //double d2 = (double)n / 2;
+            //Console.WriteLine(d2);
+            #endregion
         }
     }
 }
