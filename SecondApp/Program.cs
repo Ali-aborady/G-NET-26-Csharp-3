c27ee19c8957b06707fb794e6b7e7579035124dc
ali aborady <aboradyali48@gmail.com>
2026-02-25 02:02:34 +0200

    Question01

--- SecondApp/Program.cs
+++ SecondApp/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1: What will this print and explain what happens?
+            //=========================================================
+            //double d = 9.99;
+            //int x = (int)d;
+            //Console.WriteLine(x);  
+            //=========================================================
+
+            //double d = 9.99; int x = (int)d; Console.WriteLine(x);
+            // print 9
+            // Explanation: Explicit casting (int) truncates the decimal part
+            // It doesn't round, just removes everything after decimal point
+            #endregion
         }
     }
 }
